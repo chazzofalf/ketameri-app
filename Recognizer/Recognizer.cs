844aa6e0598802113b40f1bda5ecc5984a11a18a
Charles Montgomery <charles.montgomery@charter.net>
2025-02-15 10:35:28 -0600

    Working on Recognizer

--- Recognizer/Recognizer.cs
+++ Recognizer/Recognizer.cs
@@ -2,11 +2,57 @@
 
 namespace Recognizer;
 
+
 public class Recognizer
 {
+    public interface IRecognizedOrientation
+    {
+        public bool IsVerticallyFlipped {get;}
+        public bool IsHorizontallyFlipped {get;}
+        public bool IsRotated {get;}
+        
+        
+    }
+    private class RecognizedOrientation : IRecognizedOrientation
+    {
+        
+    
+        public bool IsVerticallyFlipped {get;}
+        public bool IsHorizontallyFlipped {get;}
+        public bool IsRotated {get;}
+        public RecognizedOrientation(bool vflip,bool hflip,bool rotate)
+        {
+            IsVerticallyFlipped = vflip;
+            IsHorizontallyFlipped = hflip;
+            IsRotated = rotate;
+        }
+    
+    }
+    private SKBitmap? lastCheckedImage;
+    private IRecognizedOrientation? lastDeterminedOrientation;
+    public IRecognizedOrientation DetermineGlyphOrientation(SKBitmap glyphImage)
+    {
+        if (glyphImage == lastCheckedImage && lastDeterminedOrientation != null)
+        {
+            return lastDeterminedOrientation;
+        }
+        else
+        {
+            
+        }
+        return null;
+    }
     public Graphic.Graphic Recognize(SKBitmap glyphImage)
     {
         if (glyphImage.Height != glyphImage.Width) throw new Exception("Ketameri Glyphs are square. This is not!");
+        var ori = DetermineGlyphOrientation(glyphImage);
+        if (ori.IsHorizontallyFlipped || ori.IsVerticallyFlipped || ori.IsRotated) throw new Exception("Out of proper orientation. Please use DetermineGlyphOrientation(...) and orient the root graphic properly first.");
+        
+        var borderless = new SKBitmap(glyphImage.Width*9/11,glyphImage.Height*9/11); // 54 is 9/11 of 66. Never forget. 
+        var c = new SKCanvas(borderless);
+        c.DrawBitmap(glyphImage,new SKRect(glyphImage.Width/11,glyphImage.Height/11,glyphImage.Width*10/11,glyphImage.Height*10/11),new SKRect(0,0,glyphImage.Width*9/11,glyphImage.Height*9/11));
+
+
         var indexedBits = Enumerable.Range(0,glyphImage.Height)
         .Select(r => Enumerable.Range(0,glyphImage.Width)
         .Select(c => (glyphImage.GetPixel(c,r).Red & 1) == 0))
