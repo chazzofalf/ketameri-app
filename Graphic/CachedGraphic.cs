446d09829050657c86bce6713c200c262dd82ae4
Charles Montgomery <charles.montgomery@charter.net>
2025-02-17 20:52:37 -0600

    Updated Compiled Glyph Cache and re-enabled it.

--- Graphic/CachedGraphic.cs
+++ Graphic/CachedGraphic.cs
@@ -54,7 +54,7 @@ public class CachedGraphic
     public static CachedGraphic  GetGraphicAtIndex(int index) => All[index];
     private static CachedGraphic[]? GenerateFromJSONCache()
     {
-        return null;
+        //return null;
         var outx = (DataClass[]?)null;
         try
         {
