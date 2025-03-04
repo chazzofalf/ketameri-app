8784da02a13d1d21be60c695623706c2656aa862
Charles Montgomery <charles.montgomery@charter.net>
2025-03-04 11:44:44 -0600

    Working on ImageViewer

--- Symbs/ImageViewer.xaml.cs
+++ Symbs/ImageViewer.xaml.cs
@@ -6,4 +6,20 @@ public partial class ImageViewer : ContentView
 	{
 		InitializeComponent();
 	}
+
+    private void SKCanvasView_PaintSurface(object sender, SkiaSharp.Views.Maui.SKPaintSurfaceEventArgs e)
+    {
+    }
+
+    private void SKCanvasView_SizeChanged(object sender, EventArgs e)
+    {
+    }
+
+    private void Load_Clicked(object sender, EventArgs e)
+    {
+    }
+
+    private void Save_Clicked(object sender, EventArgs e)
+    {
+    }
 }
