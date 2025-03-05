81a0d193ea98541e1ce2d62ef695405b91665def
Charles Montgomery <charles.montgomery@charter.net>
2025-03-04 21:50:31 -0600

    Removed Foundation Reference

--- Symbs/MainPage.xaml.cs
+++ Symbs/MainPage.xaml.cs
@@ -4,7 +4,6 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Maui.Storage;
-using Foundation;
 using Graphic;
 using Microsoft.Maui.Layouts;
 using Microsoft.VisualBasic;
